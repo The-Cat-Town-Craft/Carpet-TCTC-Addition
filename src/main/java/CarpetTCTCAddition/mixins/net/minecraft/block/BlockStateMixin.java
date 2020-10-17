53dc9359f8a5aacc1baf958a4cd43eb290bc3b64
Hendrix-Shen <3076248497@qq.com>
2020-10-17 14:27:43 +0800

    修复方块更新禁用后无法抑制更新的bug
    Signed-off-by: Hendrix-Shen <3076248497@qq.com>

--- src/main/java/CarpetTCTCAddition/mixins/net/minecraft/block/BlockStateMixin.java
+++ src/main/java/CarpetTCTCAddition/mixins/net/minecraft/block/BlockStateMixin.java
@@ -1,6 +1,7 @@
 package CarpetTCTCAddition.mixins.net.minecraft.block;
 
 import CarpetTCTCAddition.CarpetTCTCAdditionSettings;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -8,23 +9,49 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(BlockState.class)
-public class BlockStateMixin {
+public abstract class BlockStateMixin {
     @Inject(
         method = "onBlockAdded",
         at = @At(value = "HEAD"),
         cancellable = true
     )
-    private void cancelBlockAdded(World world, BlockPos pos, BlockState oldState, boolean moved, CallbackInfo ci) {
-        if(!CarpetTCTCAdditionSettings.blockUpdate) ci.cancel();
+    private void cancelBlockAdded(CallbackInfo ci) {
+        if (!CarpetTCTCAdditionSettings.blockUpdate) {
+            ci.cancel();
+        }
     }
     @Inject(
         method = "onBlockRemoved",
         at = @At(value = "HEAD"),
-            cancellable = true
+        cancellable = true
     )
-    private void cancelBlockRemove(World world, BlockPos pos, BlockState oldState, boolean moved, CallbackInfo ci) {
-        if(!CarpetTCTCAdditionSettings.blockUpdate) ci.cancel();
+    private void cancelBlockRemove(CallbackInfo ci) {
+        if (!CarpetTCTCAdditionSettings.blockUpdate) {
+            ci.cancel();
+        }
+    }
+    @Inject(
+        method = "updateNeighborStates",
+        at = @At(value = "HEAD"),
+        cancellable = true
+    )
+    private void cancelUpdateNeighborStates(CallbackInfo ci) {
+        if (!CarpetTCTCAdditionSettings.blockUpdate) {
+            ci.cancel();
+        }
+    }
+    @SuppressWarnings("UnresolvedMixinReference")
+    @Inject(
+        method = "method_11622", // neighborUpdate
+        at = @At(value = "HEAD"),
+        cancellable = true
+    )
+    private void cancelBlockAction(CallbackInfo ci) {
+        if (!CarpetTCTCAdditionSettings.blockUpdate) {
+            ci.cancel();
+        }
     }
 }
