f46c78a1daf250a0c245bcf95636a87ab0082afb
Hendrix-Shen <3076248497@qq.com>
2021-02-28 23:42:57 +0800

    1.0.6

--- src/main/java/CarpetTCTCAddition/CarpetTCTCAddition.java
+++ src/main/java/CarpetTCTCAddition/CarpetTCTCAddition.java
@@ -17,7 +17,7 @@ import java.util.Map;
 
 public class CarpetTCTCAddition implements CarpetExtension
 {
-    public static final String version = "1.0.6-dev";
+    public static final String version = "1.0.6";
     public static void noop() { }
     private static SettingsManager mySettingManager;
     static
