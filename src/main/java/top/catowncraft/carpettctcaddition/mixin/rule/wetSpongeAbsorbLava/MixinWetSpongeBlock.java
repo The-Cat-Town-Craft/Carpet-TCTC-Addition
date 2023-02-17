031bcff4b6bc92e08e6e30d8e84973dcb76bf714
Hendrix-Shen <HendrixShen@hendrixshen.top>
2023-02-17 16:54:08 +0800

    Update Copyright

--- src/main/java/top/catowncraft/carpettctcaddition/mixin/rule/wetSpongeAbsorbLava/MixinWetSpongeBlock.java
+++ src/main/java/top/catowncraft/carpettctcaddition/mixin/rule/wetSpongeAbsorbLava/MixinWetSpongeBlock.java
@@ -1,8 +1,8 @@
 /*
- * Copyright (c) Copyright 2020 - 2021 The Cat Town Craft and contributors.
- * This source code is subject to the terms of the GNU General Public
- * License, version 3. If a copy of the GPL was not distributed with this
- * file, You can obtain one at: https://www.gnu.org/licenses/gpl-3.0.txt
+ * Copyright (c) Copyright 2020 - 2023 The Cat Town Craft and contributors.
+ * This source code is subject to the terms of the GNU Lesser General Public
+ * License, version 3. If a copy of the LGPL was not distributed with this
+ * file, You can obtain one at: https://www.gnu.org/licenses/lgpl-3.0.txt
  */
 package top.catowncraft.carpettctcaddition.mixin.rule.wetSpongeAbsorbLava;
 
